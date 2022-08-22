d26d377a339900c4a99558bdb094554cf2dd9984
chcamp <c_campos_cca@hotmail.com>
2022-08-22 16:25:48 -0500

    Adicionando todos los arhivos de springBoot

---
+++ hellocd/src/test/java/com/chris/test/Test.java
@@ -0,0 +1,13 @@
+package com.chris.test;
+
+import java.io.Console;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo!! de integración continua");
+
+	}
+
+}
