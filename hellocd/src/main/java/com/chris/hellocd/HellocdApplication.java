ed6737603a8b3dd310adde2a65cc77e6088ee775
chcamp <c_campos_cca@hotmail.com>
2022-08-23 20:24:16 -0500

    dicionando un simple hello world en web app

--- hellocd/src/main/java/com/chris/hellocd/HellocdApplication.java
+++ hellocd/src/main/java/com/chris/hellocd/HellocdApplication.java
@@ -1,9 +1,38 @@
 package com.chris.hellocd;
 
+
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+public class HellocdApplication {
+
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellocdApplication.class, args);
+	}
+
+}
+
+
+
+/*
+import org.springframework.boot.SpringApplication;	
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+//@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class HellocdApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +40,5 @@ public class HellocdApplication {
 	}
 
 }
+
+*/
